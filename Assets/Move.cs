b520b788c56dd510d0bae6626c96c35ba0276e07
Nicolas VERDONCK <nverdonc@e2r12p6.42.fr>
2017-01-25 19:47:47 +0100

    Drag object rotation+move

---
+++ Assets/Move.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public Puzzle Selected;
+	public float SpeedRotate = 3;
+	public bool CanRotateHorizontaly = false;
+	public bool CanRotateVerticaly = false;
+	public bool CanMove = false;
+
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		if (Input.GetMouseButtonDown (0)) {
+			if (Physics.Raycast (Camera.main.ScreenPointToRay (Input.mousePosition), out hit, 100)) {
+				Debug.Log (hit.transform.tag);
+				if (hit.transform.tag == "Puzzle") {
+					Selected = hit.transform.gameObject.GetComponent<Puzzle>();
+				}
+			}
+		}
+		if (Input.GetMouseButtonUp (0)) {
+			Selected = null;
+		}
+		if (Selected != null) {
+			if (!Input.GetKey (KeyCode.LeftShift)) {
+				if (CanRotateHorizontaly && !Input.GetKey (KeyCode.LeftControl))
+					Selected.transform.Rotate (new Vector3 (0, -Input.GetAxis ("Mouse X"), 0));
+				if (CanRotateVerticaly && Input.GetKey (KeyCode.LeftControl))
+					Selected.transform.Rotate (new Vector3 (-Input.GetAxis ("Mouse Y"), 0, 0));
+			} else if (CanMove && Input.GetKey (KeyCode.LeftShift)) {
+				Selected.transform.Translate (new Vector3 (-Input.GetAxis ("Mouse X") / 4, Input.GetAxis ("Mouse Y") / 4, 0));
+			}
+		}
+	}
+}
